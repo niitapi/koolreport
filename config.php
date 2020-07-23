38339cf3b3e3acc62156bea9d7db29f3a371768b
niitapi <niitapiintegration@gmail.com>
2020-07-23 13:38:32 +0530

    CS

--- config.php
+++ config.php
@@ -7,21 +7,21 @@ return array(
         "charset"=>"utf8"
     ),
     "sakila"=>array(
-        "connectionString"=>"mysql:host=sampledb.koolreport.com;dbname=sakila",
-        "username"=>"expusr",
-        "password"=>"koolreport sampledb",
+        "connectionString"=>"mysql:host=10.10.18.155;dbname=sakila",
+        "username"=>"root",
+        "password"=>"StgDW#321",
         "charset"=>"utf8"
     ),
     "world"=>array(
-        "connectionString"=>"mysql:host=sampledb.koolreport.com;dbname=world",
-        "username"=>"expusr",
-        "password"=>"koolreport sampledb",
+        "connectionString"=>"mysql:host=10.10.18.155;dbname=world",
+        "username"=>"root",
+        "password"=>"StgDW#321",
         "charset"=>"utf8"
     ),  
     "employees"=>array(
-        "connectionString"=>"mysql:host=sampledb.koolreport.com;dbname=employees",
-        "username"=>"expusr",
-        "password"=>"koolreport sampledb",
+        "connectionString"=>"mysql:host=10.10.18.155;dbname=employees",
+        "username"=>"root",
+        "password"=>"StgDW#321",
         "charset"=>"utf8"
     ),  
     "salesCSV"=>array(
